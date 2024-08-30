2cd314df0254f98ca902a2c6f90c730cdbe67ad4
jjinwooya <jjinwoo1987@naver.com>
2024-08-30 14:15:51 +0900

    템플릿 수정 3 프로젝트

--- portfolio/src/main/kotlin/com/jinwoo/portfolio/domain/DataInitializer.kt
+++ portfolio/src/main/kotlin/com/jinwoo/portfolio/domain/DataInitializer.kt
@@ -75,50 +75,76 @@ class DataInitializer(
         skillRepository.saveAll(mutableListOf(java, kotlin,spring,mysql,oracle))
 
         val project1 = Project(
-            name = "대학",
-            description = "전공",
+            name = "부기무비(팀 프로젝트)",
+            description = "영화예매사이트",
             startYear = 2024,
-            startMonth = 9,
+            startMonth = 4,
             endYear = 2024,
-            endMonth = 12,
+            endMonth = 5,
             isActive = true
         )
         project1.addDetails(
             mutableListOf(
-                ProjectDetail(content = "평점", url = null, isActive = true),
-                ProjectDetail(content = "쿠오오", url = null, isActive = true),
+                ProjectDetail(content = "영화 페이지 및 스토어 페이지 담당", url = null, isActive = true),
+                ProjectDetail(content = "id= jinwoo1234 pw= 123456a! ", url = null, isActive = true),
+                ProjectDetail(content = "부기무비 바로가기", url = "http://c3d2401t2.itwillbs.com/boogimovie", isActive = true)
             )
         )
         project1.skills.addAll(
             mutableListOf(
+                ProjectSkill(project = project1, skill = mysql),
                 ProjectSkill(project = project1, skill = java),
-                ProjectSkill(project = project1, skill = kotlin)
-            )
+                ProjectSkill(project = project1, skill = spring)
+                )
         )
         val project2 = Project(
-            name = "새대학",
-            description = "새전공",
+            name = "클래스윌(팀 프로젝트)",
+            description = "원데이 수업 클래스",
             startYear = 2024,
-            startMonth = 9,
+            startMonth = 5,
             endYear = 2024,
-            endMonth = 12,
+            endMonth = 7,
             isActive = true
         )
-        project1.addDetails(
+        project2.addDetails(
             mutableListOf(
-                ProjectDetail(content = "새평점", url = null, isActive = true),
-                ProjectDetail(content = "새쿠오오", url = null, isActive = true),
-                ProjectDetail(content = "네이버", url = "www.naver.com", isActive = true)
+                ProjectDetail(content = "마이페이지 담당", url = null, isActive = true),
+                ProjectDetail(content = "id= jinwoo@naver.com pw= 12345a ", url = null, isActive = true),
+                ProjectDetail(content = "클래스윌 바로가기", url = "http://c3d2401t2.itwillbs.com/class-will", isActive = true)
             )
         )
         project2.skills.addAll(
             mutableListOf(
+                ProjectSkill(project = project2, skill = mysql),
                 ProjectSkill(project = project2, skill = java),
-                ProjectSkill(project = project2, skill = kotlin),
+                ProjectSkill(project = project2, skill = spring)
 
                 )
         )
-        projectRepository.saveAll(mutableListOf(project1, project2))
+        val project3 = Project(
+            name = "간단한 책 대여 (리눅스 및 아마존 EC2사용)",
+            description = "리눅스 환경에서 아마존 EC2를 이용한 배포 및 가비아 도메인을 사용한 연결 프로젝트",
+            startYear = 2024,
+            startMonth = 8,
+            endYear = 2024,
+            endMonth = 8,
+            isActive = true
+        )
+        project3.addDetails(
+            mutableListOf(
+                ProjectDetail(content = "리눅스 환경에서 아마존 EC2를 이용하여 배포", url = null, isActive = true),
+                ProjectDetail(content = "가비아를 바탕으로 도메인 연결", url = null, isActive = true),
+                ProjectDetail(content = "큰 기능은 없고 개인공부삼아 해봤습니다.", url = null, isActive = true),
+                ProjectDetail(content = "책대여 바로가기", url = "http://www.jinwoo.pro:8080", isActive = true)
+            )
+        )
+        project3.skills.addAll(
+            mutableListOf(
+                ProjectSkill(project = project3, skill = java),
+                ProjectSkill(project = project3, skill = spring)
+            )
+        )
+        projectRepository.saveAll(mutableListOf(project1, project2,project3))
 
     }
 
