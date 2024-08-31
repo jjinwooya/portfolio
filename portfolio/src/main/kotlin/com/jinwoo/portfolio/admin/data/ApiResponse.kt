c95eeb8268e5ed3a27e1a5ae3e72071f512c7ff2
jjinwooya <jjinwoo1987@naver.com>
2024-08-31 12:49:01 +0900

    Dto 개발

--- portfolio/src/main/kotlin/com/jinwoo/portfolio/admin/data/ApiResponse.kt
+++ portfolio/src/main/kotlin/com/jinwoo/portfolio/admin/data/ApiResponse.kt
@@ -3,4 +3,18 @@ package com.jinwoo.portfolio.admin.data
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
-class ApiResponse<T>(status: HttpStatus) : ResponseEntity<T>(status)
+class ApiResponse<T>(status: HttpStatus) : ResponseEntity<T>(status) {
+    companion object {
+        fun successCreate(): ResponseEntity<Any> {
+            return ok("데이터저장")
+        }
+
+        fun successUpdate(): ResponseEntity<Any> {
+            return ok("데이터수정")
+        }
+
+        fun successDelete(): ResponseEntity<Any> {
+            return ok("데이터삭제")
+        }
+    }
+}
--- portfolio/src/main/kotlin/com/jinwoo/portfolio/admin/data/FormElementDTO.kt
+++ portfolio/src/main/kotlin/com/jinwoo/portfolio/admin/data/FormElementDTO.kt
@@ -5,3 +5,22 @@ abstract class FormElementDTO(
     val size: Int,
     val type: String
 )
+
+class TextFormElementDTO(
+    name: String,
+    size: Int
+) : FormElementDTO(name = name, size = size, type = "Text")
+
+class DateFormElementDTO(
+    name: String,
+    size: Int
+) : FormElementDTO(name = name, size = size, type = "Date")
+
+class SelectFormElementDTO(
+    name: String,
+    size: Int,
+    options: List<Any>
+) : FormElementDTO(name = name, size = size, type = "Select") {
+    val options: List<Any> = options
+
+}
